146f8dfeef8a2a3c8a71a19f12be7de307c72f33
Mizuki Okushima <caresupplement@icloud.com>
2025-05-09 15:36:25 +0900

    forth commit

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import AuthProvider from "./lessons/Lesson4/Lesson4_1/context/AuthContext.tsx";
 
 //https://ja.react.dev/reference/react/useState#updating-state-based-on-the-previous-state
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   // StrictMode 開発環境のみ二回呼び出される
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
